c500fc8dda2423254382daee1baff94acaa141aa
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-02-13 20:10:29 +0100

    patterngen writer: support --metrics

--- seal5/backends/patterngen/writer.py
+++ seal5/backends/patterngen/writer.py
@@ -18,6 +18,7 @@ from typing import Union
 from m2isar.metamodel import arch
 
 from seal5.tools import cdsl2llvm
+from seal5.model import Seal5InstrAttribute
 
 logger = logging.getLogger("patterngen_tablegen_writer")
 
@@ -31,6 +32,7 @@ def main():
     parser.add_argument("--log", default="info", choices=["critical", "error", "warning", "info", "debug"])
     parser.add_argument("--output", "-o", type=str, default=None)
     parser.add_argument("--splitted", action="store_true", help="Split per set and instruction")
+    parser.add_argument("--metrics", default=None, help="Output metrics to file")
     parser.add_argument("--ext", type=str, default="td", help="Default file extension (if using --splitted)")
     args = parser.parse_args()
 
@@ -74,26 +76,52 @@ def main():
             else:
                 assert False
 
+    metrics = {
+        "n_sets": 0,
+        "n_instructions": 0,
+        "n_skipped": 0,
+        "n_failed": 0,
+        "n_success": 0,
+    }
     # preprocess model
     # print("model", model)
     if args.splitted:
         # errs = []
         assert out_path.is_dir(), "Expecting output directory when using --splitted"
         for set_name, set_def in model["sets"].items():
+            metrics["n_sets"] += 1
+            ext_settings = set_def.settings
             for instr_def in set_def.instructions.values():
+                metrics["n_instructions"] += 1
                 input_file = out_path / set_name / f"{instr_def.name}.core_desc"
+                attrs = instr_def.attributes
+                if len(attrs) > 0:
+                    skip = False
+                    if Seal5InstrAttribute.MAY_LOAD in attrs:
+                        skip = True
+                    elif Seal5InstrAttribute.MAY_STORE in attrs:
+                        skip = True
+                    elif arch.InstrAttribute.COND in attrs:
+                        skip = True
+                    elif arch.InstrAttribute.NO_CONT in attrs:
+                        skip = True
+                    if skip:
+                        metrics["n_skipped"] += 1
+                        continue
                 if not input_file.is_file():
-                    pass  # skipping
-                    # errs.append(TODO)
+                    metrics["n_skipped"] += 1
+                    continue
                 output_file = out_path / set_name / f"{instr_def.name}.{args.ext}"
                 install_dir = os.getenv("CDSL2LLVM_DIR", None)
-                ext = None  # TODO: pass via instruction attributes
+                ext = ext_settings.feature
+                assert ext is not None
                 assert install_dir is not None
                 install_dir = pathlib.Path(install_dir)
                 try:
                     cdsl2llvm.run_pattern_gen(install_dir / "llvm" / "build", input_file, output_file, skip_patterns=False, skip_formats=not args.formats, ext=ext)
+                    metrics["n_success"] += 1
                 except AssertionError:
-                    pass
+                    metrics["n_failed"] += 1
                     # errs.append((insn_name, str(ex)))
         # if len(errs) > 0:
         #     # print("errs", errs)
@@ -105,6 +133,18 @@ def main():
         #         out_path_.parent.mkdir(exist_ok=True)
     else:
         raise NotImplementedError
+    if args.metrics:
+        metrics_file = args.metrics
+        with open(metrics_file, "w") as f:
+            f.write(",".join(metrics.keys()))
+            f.write("\n")
+            f.write(",".join(map(str, metrics.values())))
+            f.write("\n")
+    if args.index:
+        index_data = "TODO"
+        index_file = args.index
+        with open(index_file, "w") as f:
+            f.write(index_data + "\n")
 
 
 if __name__ == "__main__":
