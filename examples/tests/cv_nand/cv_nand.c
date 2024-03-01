19dea0b6df01246f6e977285acd52b9461ff03ce
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-03-01 14:05:51 +0100

    update cv_nand tests

--- examples/tests/cv_nand/cv_nand.c
+++ examples/tests/cv_nand/cv_nand.c
@@ -1,15 +1,15 @@
-// RUN: clang -cc1 -triple riscv32 -target-feature +m -target-feature +xcvnand -S -O3 %s -o - \
+// RUN: clang -cc1 -triple riscv32 -target-feature +m -target-feature +xcorevnand -mllvm -global-isel=1 -S -O3 %s -o - \
 // RUN:     | FileCheck %s  -check-prefix=CHECK
 
 // CHECK-LABEL: nand_bitwise_s32:
-// CHECK-COUNT-1: cv.nand_bitwise {{.*}}
+// CHECK-COUNT-1: cv.nand.bitwise {{.*}}
 signed int nand_bitwise_s32(signed int a, signed int b)
 {
   return ~(a & b);
 }
 
 // CHECK-LABEL: nand_bitwise_u32:
-// CHECK-COUNT-1: cv.nand_bitwise {{.*}}
+// CHECK-COUNT-1: cv.nand.bitwise {{.*}}
 unsigned int nand_bitwise_u32(unsigned int a, unsigned int b)
 {
   return ~(a & b);
--- examples/tests/cv_nand/cv_nand.s
+++ examples/tests/cv_nand/cv_nand.s
@@ -1,9 +1,9 @@
-# RUN: llvm-mc %s -triple=riscv32 -mattr=+xcvnand -riscv-no-aliases -show-encoding \
+# RUN: llvm-mc %s -triple=riscv32 -mattr=+xcorevnand -riscv-no-aliases -show-encoding \
 # RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
-# RUN: llvm-mc -filetype=obj -triple=riscv32 -mattr=+xcvnand < %s \
-# RUN:     | llvm-objdump --mattr=+xcvnand -M no-aliases -d -r - \
+# RUN: llvm-mc -filetype=obj -triple=riscv32 -mattr=+xcorevnand < %s \
+# RUN:     | llvm-objdump --mattr=+xcorevnand -M no-aliases -d -r - \
 # RUN:     | FileCheck --check-prefix=CHECK-ASM-AND-OBJ %s
 
-# CHECK-ASM-AND-OBJ: cv.nand_bitwise a4, ra, s0
+# CHECK-ASM-AND-OBJ: cv.nand.bitwise a4, ra, s0
 # CHECK-ASM: encoding: [0x2b,0xe7,0x80,0x92]
-cv.nand_bitwise a4, ra, s0
+cv.nand.bitwise a4, ra, s0
