6f701b69e370fbc4e584e4b9fa4fa667de94c92b
jokap11 <ge25der@mytum.de>
2024-06-03 13:22:26 +0200

    demo script uses now build directory working on Ubuntu 22.10

--- examples/demo.sh
+++ examples/demo.sh
@@ -5,26 +5,34 @@ set -e
 
 Example_files=examples/cdsl/rv_example/Example.core_desc
 Config_files=examples/cfg/*.yml
+DEST=${DEST:-/tmp/var/seal5_demo}
 
+Config_files=examples/cfg/*.yml
+
+echo Config_files:;
 echo ${Config_files};
+echo Example_files:;
+echo ${Example_files};
+echo Build destination:;
+echo ${DEST};
 
-seal5 reset --verbose --settings;
-seal5 clean --temp --patches --models --inputs --verbose;
-seal5 init --verbose;
-seal5 load --files ${Example_files};
-seal5 load --files ${Config_files};
-seal5 setup --verbose;
-seal5 patch -s 0 --verbose;
-seal5 build  --verbose;
-seal5 transform --verbose;
-seal5 generate --skip patter_gen --verbose;
-seal5 patch -s 1 2 --verbose;
-seal5 build  --verbose;
-seal5 build  --verbose -t pattern-gen;
-seal5 build  --verbose -t llc;
-seal5 generate --only patter_gen --verbose;
-seal5 patch -s 3 4 5 --verbose;
-seal5 build --verbose;
-seal5 test --verbose;
-seal5 deploy --verbose;
-seal5 export --verbose;
+seal5 reset --verbose --settings -dir DEST;
+seal5 clean --temp --patches --models --inputs --verbose -dir DEST;
+seal5 init --verbose -dir DEST;
+seal5 load --files ${Example_files} -dir DEST;
+seal5 load --files ${Config_files} -dir DEST;
+seal5 setup --verbose -dir DEST;
+seal5 patch -s 0 --verbose -dir DEST;
+seal5 build  --verbose -dir DEST;
+seal5 transform --verbose -dir DEST;
+seal5 generate --skip patter_gen --verbose -dir DEST;
+seal5 patch -s 1 2 --verbose -dir DEST;
+seal5 build  --verbose -dir DEST;
+seal5 build  --verbose -t pattern-gen -dir DEST;
+seal5 build  --verbose -t llc -dir DEST;
+seal5 generate --only patter_gen --verbose -dir DEST;
+seal5 patch -s 3 4 5 --verbose -dir DEST;
+seal5 build --verbose -dir DEST;
+seal5 test --verbose -dir DEST;
+seal5 deploy --verbose -dir DEST;
+seal5 export --verbose -dir DEST;
