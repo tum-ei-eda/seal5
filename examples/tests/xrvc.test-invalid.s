5f54bfa0b3c51c0eb96793165a582c742da7cc2b
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-06-04 13:53:47 +0200

    example: update xrvc tests to include imm overflow checks

--- examples/tests/xrvc.test-invalid.s
+++ examples/tests/xrvc.test-invalid.s
@@ -3,8 +3,12 @@
 
 xrv.nand a0, a1, 0 # CHECK-ERROR: invalid operand for instruction
 
-xrv.nandi a0, a1, a2 # CHECK-ERROR: invalid operand for instruction
+xrv.nandi a0, a1, 5555 # CHECK-ERROR: operand must be a symbol with %lo/%pcrel_lo/%tprel_lo modifier or an integer in the range [-2048, 2047]
+
+xrv.nandi a0, a1, a2 # CHECK-ERROR: operand must be a symbol with %lo/%pcrel_lo/%tprel_lo modifier or an integer in the range [-2048, 2047]
 
 xrvc.nand a0, 0 # CHECK-ERROR: invalid operand for instruction
 
-xrvc.nandi a0, a1 # CHECK-ERROR: invalid operand for instruction
+xrvc.nandi a0, 36 # CHECK-ERROR: immediate must be an integer in the range [-32, 31]
+
+xrvc.nandi a0, a1 # CHECK-ERROR: immediate must be an integer in the range [-32, 31]
