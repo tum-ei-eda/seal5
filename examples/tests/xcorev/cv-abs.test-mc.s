d7bbc39919890d245fc10452789cc7ee2cf8d80b
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-06-10 16:16:19 +0200

    examples: add more xcvalu codegen tests

--- examples/tests/xcorev/cv-abs.test-mc.s
+++ examples/tests/xcorev/cv-abs.test-mc.s
@@ -1,10 +1,10 @@
-# RUN: llvm-mc -triple=riscv32 --mattr=+xcvalu -show-encoding %s \
+# RUN: llvm-mc -triple=riscv32 --mattr=+xcorevalu -show-encoding %s \
 # RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INSTR
 
-cv.abs t0, t1
-# CHECK-INSTR: cv.abs t0, t1
+seal5.cv.abs t0, t1
+# CHECK-INSTR: seal5.cv.abs t0, t1
 # CHECK-ENCODING: [0xab,0x32,0x03,0x50]
 
-cv.abs a0, a1
-# CHECK-INSTR: cv.abs a0, a1
+seal5.cv.abs a0, a1
+# CHECK-INSTR: seal5.cv.abs a0, a1
 # CHECK-ENCODING: [0x2b,0xb5,0x05,0x50]
--- examples/tests/xcorev/cv_mac.inline_asm.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang --target=riscv32 -march=rv32xcorevmac -c -o %t.o %s
-// RUN: llvm-objdump --disassembler-options=numeric -d %t.o | FileCheck %s
-
-int main() {
-  // CHECK: 0b 90 1a 01 seal5.cv.mac x1, x2, x3
-  asm("seal5.cv.mac x1, x2, x3");
-
-  return 0;
-}
