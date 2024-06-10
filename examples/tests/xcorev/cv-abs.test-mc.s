1ad007d64e439c0195185fe5c2ac8a987832d349
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-06-10 06:54:16 +0200

    examples: add cv-abs test files

---
+++ examples/tests/xcorev/cv-abs.test-mc.s
@@ -0,0 +1,10 @@
+# RUN: llvm-mc -triple=riscv32 --mattr=+xcvalu -show-encoding %s \
+# RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INSTR
+
+cv.abs t0, t1
+# CHECK-INSTR: cv.abs t0, t1
+# CHECK-ENCODING: [0xab,0x32,0x03,0x50]
+
+cv.abs a0, a1
+# CHECK-INSTR: cv.abs a0, a1
+# CHECK-ENCODING: [0x2b,0xb5,0x05,0x50]
