8b34740b5cbc6f8d7e2d9fd9be8dee823107cec7
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-01 15:54:29 +0200

    allow shallow clone of llvm repo

--- seal5/tools/llvm.py
+++ seal5/tools/llvm.py
@@ -100,7 +100,9 @@ def clone_llvm_repo(
         if depth is not None and ref is not None:
             assert "llvmorg" in ref  # Needs to be a valid branch name, not a tag
             branch = ref
-        repo = git.Repo.clone_from(clone_url, dest, no_checkout=no_checkout, branch=branch, depth=depth, progress=clone_progress)
+        repo = git.Repo.clone_from(
+            clone_url, dest, no_checkout=no_checkout, branch=branch, depth=depth, progress=clone_progress
+        )
         if ref:
             logger.debug("Checking out branch: %s", ref)
             repo.git.checkout(ref)
