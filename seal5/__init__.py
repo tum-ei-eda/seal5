463da3d17b209f5953ed41503f3a61c9e5f44719
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-16 15:15:41 +0200

    add missing license headers and file docstrings

--- seal5/__init__.py
+++ seal5/__init__.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""TODO."""
+"""Seal5 Project."""
--- seal5/backends/riscv_register_info/__init__.py
+++ seal5/backends/riscv_register_info/__init__.py
@@ -16,8 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""MAKO templates for RISCVISAInfo.tf patches"""
-
-import pathlib
-
-template_dir = pathlib.Path(__file__).parent.resolve()
+"""Seal5 RISCVRegisterInfo.td backend"""
