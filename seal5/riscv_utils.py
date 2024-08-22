cf365f5d973a28fcad7defbdf61f15490a1d49d2
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-22 13:01:44 +0200

    add missing file: /work/git/seal5_new/seal5/riscv_utils.py

---
+++ seal5/riscv_utils.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+
+def get_riscv_defaults(riscv_settings):
+    default_features = ["+m", "+fast-unaligned-access"]
+    default_xlen = None
+    if riscv_settings:
+        default_features_ = riscv_settings.features
+        if default_features_ is not None:
+            default_features = default_features_
+        default_xlen_ = riscv_settings.xlen
+        if default_xlen is not None:
+            default_xlen = default_xlen_
+    return default_features, default_xlen
+
+
+def build_mattr(features, xlen: Optional[int] = None):
+    mattrs = []
+
+    def fix_prefix(x):
+        assert len(x) > 0
+        if x[0] in ["-", "+"]:
+            return x
+        return f"+{x}"
+
+    if features is not None:
+        mattrs += [f"+{f}" for f in features]
+    if xlen is not None:
+        if xlen == 64:
+            mattrs += ["+64bit"]
+    mattrs = list(set(map(fix_prefix, mattrs)))
+    mattr = ",".join(mattrs)
+    return mattr
