868503ca4b1287de408e319f6c7ced0fbf3200ce
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-10-25 11:53:42 +0200

    add example openasip test files (see #129)

---
+++ examples/tests/openasip/base/openasip_base_max.test-cg.c
@@ -0,0 +1,11 @@
+// RUN: clang -c -target riscv32-unknown-elf -march=rv32i -Xclang -target-feature -Xclang +xopenasipbase -o %t.o %s
+// RUN: llvm-objdump --mattr=+i,+xopenasipbase --disassembler-options=numeric -d %t.o | FileCheck %s
+
+int test_max(int a, int b) {
+    // CHECK: 8b 8a b5 2b openasip_base_max x21, x11, x27
+    return (a > b) ? a : b;
+}
+
+// unsigned int test_max2(unsigned int a, unsigned int b) {
+//     return (a > b) ? a : b;
+// }
