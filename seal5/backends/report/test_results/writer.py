e673a26249b1b6b986338d9faf53bff3cbb09041
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-10-31 09:13:34 +0100

    test_results/writer.py: support multiple models & fix test detection

--- seal5/backends/report/test_results/writer.py
+++ seal5/backends/report/test_results/writer.py
@@ -38,42 +38,6 @@ def main():
     # initialize logging
     logging.basicConfig(level=getattr(logging, args.log.upper()))
 
-    # resolve model paths
-    top_level = pathlib.Path(args.top_level)
-    # abs_top_level = top_level.resolve()
-
-    is_seal5_model = False
-    if top_level.suffix == ".seal5model":
-        is_seal5_model = True
-    # if args.output is None:
-    #     assert top_level.suffix in [".m2isarmodel", ".seal5model"], "Can not infer model type from file extension."
-    #     raise NotImplementedError
-
-    #     # out_path = top_level.parent / (top_level.stem + ".core_desc")
-    # else:
-    assert args.output is not None
-    out_path = pathlib.Path(args.output)
-
-    logger.info("loading models")
-    if not is_seal5_model:
-        raise NotImplementedError
-
-    # load models
-    with open(top_level, "rb") as f:
-        # models: "dict[str, arch.CoreDef]" = pickle.load(f)
-        if is_seal5_model:
-            model: "dict[str, Union[arch.InstructionSet, ...]]" = pickle.load(f)
-            model["cores"] = {}
-        else:  # TODO: core vs. set!
-            temp: "dict[str, Union[arch.InstructionSet, arch.CoreDef]]" = pickle.load(f)
-            assert len(temp) > 0, "Empty model!"
-            if isinstance(list(temp.values())[0], arch.CoreDef):
-                model = {"cores": temp, "sets": {}}
-            elif isinstance(list(temp.values())[0], arch.InstructionSet):
-                model = {"sets": temp, "cores": {}}
-            else:
-                assert False
-
     assert args.yaml is not None
     assert pathlib.Path(args.yaml).is_file()
     settings = Seal5Settings.from_yaml_file(args.yaml)
@@ -131,8 +95,18 @@ def main():
 
     def filter_tests(tests, instr_name: str):
         assert isinstance(tests, dict)
-        instr_name_alt = instr_name.replace(".", "-")
-        candidates = {instr_name, instr_name_alt, instr_name.lower(), instr_name_alt.lower()}
+        candidates = set()
+        candidates.add(instr_name)
+        candidates.add(instr_name.lower())
+        instr_name_alt = instr_name.replace("seal5.", "").replace("SEAL5.", "")
+        instr_name_alt = instr_name_alt.replace("seal5_", "").replace("SEAL5_", "")
+        instr_name_alt = instr_name_alt.replace(".", "-")
+        candidates.add(instr_name_alt)
+        candidates.add(instr_name_alt.lower())
+        instr_name_alt = instr_name_alt.replace("_", "-")
+        candidates.add(instr_name_alt)
+        candidates.add(instr_name_alt.lower())
+
         ret = []
         for candidate_name in candidates:
             ret_ = tests.get(candidate_name, None)
@@ -142,29 +116,68 @@ def main():
         return ret
 
     results_data = []
-    for set_name, set_def in model["sets"].items():
-        xlen = set_def.xlen
-        model = top_level.stem
+    # resolve model paths
+    top_levels = args.top_level
+    for top_level in top_levels:
+        top_level = pathlib.Path(top_level)
+        # abs_top_level = top_level.resolve()
 
-        for instr_def in set_def.instructions.values():
-            instr_name = instr_def.name
+        is_seal5_model = False
+        if top_level.suffix == ".seal5model":
+            is_seal5_model = True
+        # if args.output is None:
+        #     assert top_level.suffix in [".m2isarmodel", ".seal5model"], "Can not infer model type from file extension."
+        #     raise NotImplementedError
 
-            data = {
-                "model": model,
-                "set": set_name,
-                "xlen": xlen,
-                "instr": instr_name,
-            }
-            found_tests = filter_tests(tests, instr_name)
-            for test_kind, test_file, test_fmt, test_result in found_tests:
-                data_ = {
-                    **data,
-                    "test_kind": test_kind,
-                    "test_file": test_file,
-                    "test_fmt": test_fmt,
-                    "result": test_result,
+        #     # out_path = top_level.parent / (top_level.stem + ".core_desc")
+        # else:
+        assert args.output is not None
+        out_path = pathlib.Path(args.output)
+
+        logger.info("loading models")
+        if not is_seal5_model:
+            raise NotImplementedError
+
+        # load models
+        with open(top_level, "rb") as f:
+            # models: "dict[str, arch.CoreDef]" = pickle.load(f)
+            if is_seal5_model:
+                model: "dict[str, Union[arch.InstructionSet, ...]]" = pickle.load(f)
+                model["cores"] = {}
+            else:  # TODO: core vs. set!
+                temp: "dict[str, Union[arch.InstructionSet, arch.CoreDef]]" = pickle.load(f)
+                assert len(temp) > 0, "Empty model!"
+                if isinstance(list(temp.values())[0], arch.CoreDef):
+                    model = {"cores": temp, "sets": {}}
+                elif isinstance(list(temp.values())[0], arch.InstructionSet):
+                    model = {"sets": temp, "cores": {}}
+                else:
+                    assert False
+
+
+        for set_name, set_def in model["sets"].items():
+            xlen = set_def.xlen
+            model = top_level.stem
+
+            for instr_def in set_def.instructions.values():
+                instr_name = instr_def.name
+
+                data = {
+                    "model": model,
+                    "set": set_name,
+                    "xlen": xlen,
+                    "instr": instr_name,
                 }
-                results_data.append(data_)
+                found_tests = filter_tests(tests, instr_name)
+                for test_kind, test_file, test_fmt, test_result in found_tests:
+                    data_ = {
+                        **data,
+                        "test_kind": test_kind,
+                        "test_file": test_file,
+                        "test_fmt": test_fmt,
+                        "result": test_result,
+                    }
+                    results_data.append(data_)
     results_df = pd.DataFrame(results_data)
     fmt = args.fmt
     if fmt == "auto":
