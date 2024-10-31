6b86d7f334dc05754aa8a5364403b4e74a7b9d10
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-10-31 11:05:19 +0100

    fix tests

--- examples/tests/xopenasip/base/openasip_base_max.test-cg.c
+++ examples/tests/xopenasip/base/openasip_base_max.test-cg.c
@@ -1,8 +1,8 @@
-// RUN: clang -c -target riscv32-unknown-elf -march=rv32i -Xclang -target-feature -Xclang +xopenasipbase -mllvm -global-isel=1 -o %t.o %s
+// RUN: clang -c -target riscv32-unknown-elf -march=rv32i -Xclang -target-feature -Xclang +xopenasipbase -mllvm -global-isel=1 -O3 -o %t.o %s
 // RUN: llvm-objdump --mattr=+i,+xopenasipbase --disassembler-options=numeric -d %t.o | FileCheck %s
 
 int test_max(int a, int b) {
-    // CHECK: 8b 8a b5 2b openasip_base_max x21, x11, x27
+    // CHECK: 0b 05 b5 2a openasip_base_max x10, x10, x11
     return (a > b) ? a : b;
 }
 
