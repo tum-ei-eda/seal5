52243510c1857786f9fc273b23a7a0f6511f351f
kebi-be <134584807+kebi-be@users.noreply.github.com>
2024-12-19 10:34:17 +0100

    Update merge_test_artifacts.py

--- examples/merge_test_artifacts.py
+++ examples/merge_test_artifacts.py
@@ -116,14 +116,11 @@ def main():
        perc_result = '['+ round(perc_pass,2).astype(str) +'%' + ' / '+ round(perc_fail,2).astype(str) +'%'+ ']'
        return perc_result;
 
-
-
-    stat_prop_result_cv_test["Summary Test Results: (Passed/Failed) % "] =  stat_prop_result_cv_test["n_pass"].astype(str)+' / '+ stat_prop_result_cv_test["n_fail"].astype(str)+ ' ' + calc_test_percentage()
-
     stat_result_summary =  stat_prop_result_cv_df2["n_success"].astype(str)+' / '+ stat_prop_result_cv_df2["n_skipped"].astype(str) +' / '+ stat_prop_result_cv_df2["n_failed"].astype(str)+ ' ' + calc_stage_percentage2()
-
     stat_prop_result_cv_df2.insert(loc=1, column='Status_Summary: (Passed/Skipped/Failed) %', value=stat_result_summary)
 
+    stat_prop_test_summary = stat_prop_result_cv_test["n_pass"].astype(str)+' / '+ stat_prop_result_cv_test["n_fail"].astype(str)+ ' ' + calc_test_percentage()
+    stat_prop_result_cv_df2.insert(loc=0, column=' Summary Test Results: (Passed/Failed) % ', value=stat_prop_test_summary)
 
     #'n_exists', 'n_required' , 'n_optional', 'n_extra', 'n_required_exists', 'n_optional_exists'
 
@@ -133,6 +130,17 @@ def main():
    
     final_test_table = pd.merge(final_status_results, final_test_results , on=['model', 'enc_format', 'opcode'], how='inner')
     final_coverage_table = pd.merge(final_test_table, final_coverage_results , on=['model', 'enc_format', 'opcode'], how='inner')
+
+    save_data_frames_as_html_to_file(final_test_table, "Grouped_stat_prop_result_test.html")
+    save_data_frames_as_html_to_file(final_coverage_table, "Grouped_stat_prop_result_cv.html")
+    save_data_frames_as_html_to_file(stat_prop_result_cv_df, "Grouped_stat_prop_result_all.html")
+
+
+    final_test_table = final_coverage_table.drop('instr', axis=1)
+    final_coverage_table = final_coverage_table.drop('instr', axis=1)
+    save_data_frames_as_html_to_file(final_test_table, "Grouped_stat_prop_result_test_wo_instr.html")
+    save_data_frames_as_html_to_file(final_coverage_table, "Grouped_stat_prop_result_cv_wo_instr.html")
+    
     print(final_test_results)
     print(final_status_results)
 
