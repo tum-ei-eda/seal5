b9879bc4def7300479dbf77386f4a8dfe26d7a3d
kebi-be <134584807+kebi-be@users.noreply.github.com>
2024-12-27 11:58:33 +0100

    Added code for coverage results

--- examples/merge_test_artifacts.py
+++ examples/merge_test_artifacts.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import pathlib
 import math
@@ -42,13 +40,16 @@ def main():
     else:
        print('Path to Test Coverage file must be specified')    
 
+    
     status_props_df = pd.merge(status_df, props_df)
     stat_prop_result_df = pd.merge(status_props_df, test_result_df, on=['model', 'set', 'xlen', 'instr'], how="left")
 
     stat_prop_result_df = stat_prop_result_df.sort_values("model")
+   
     stat_prop_result_df.fillna(value = 0, 
           inplace = True)
-     
+
+
     stat_prop_result_cv_df = pd.merge( stat_prop_result_df, test_cv_df, on=['model', 'set', 'xlen', 'instr'], how='left')
 
     def save_data_frames_as_html_to_file(dataframe, filename):
@@ -56,24 +57,8 @@ def main():
             print(dataframe.to_html(buf=html_file, index=True))
 
 
-    def calc_stage_percentage( ):
-       perc_success = (((stat_prop_result_cv_df['n_success'] *100) / stat_prop_result_cv_df['n_total']).astype(int))
-       perc_skipped = (((stat_prop_result_cv_df['n_skipped'] *100) / stat_prop_result_cv_df['n_total']).astype(int))
-       perc_failed  = (((stat_prop_result_cv_df['n_failed'] *100) / stat_prop_result_cv_df['n_total']).astype(int))
-
-       perc_result = '['+ round(perc_success,2).astype(str) +'%' + ' / '+ round(perc_skipped,2).astype(str) +'%'+ ' / '+ round(perc_failed,2).astype(str) +'%'+ ']'
-       return perc_result;
-
-    def calc_stage_percentage_sum( ):
-       perc_success = (((stat_prop_result_cv_df['n_success'].sum() *100) / stat_prop_result_cv_df['n_total'].sum() ).astype(int))
-       perc_skipped = (((stat_prop_result_cv_df['n_skipped'].sum() *100) / stat_prop_result_cv_df['n_total'].sum() ).astype(int))
-       perc_failed  = (((stat_prop_result_cv_df['n_failed'].sum() *100) / stat_prop_result_cv_df['n_total'].sum() ).astype(int))
-
-       perc_result = '['+ round(perc_success,2).astype(str)  +'%' + ' / '+ round(perc_skipped,2).astype(str) +'%'+ ' / '+ round(perc_failed,2).astype(str)  +'%'+ ']'
-       return perc_result;
-
     def rounded_mean(x):
-        return x.mean().round(1);
+        return x.mean().round(0).astype(int);
        
     stat_prop_result_cv_df.set_index(['model', 'enc_format', 'opcode'], inplace=True)
     
@@ -81,7 +66,7 @@ def main():
     
     stat_prop_result_cv_test = stat_prop_result_cv_df.groupby(level=[0,1,2])[['n_pass', 'n_fail',  'n_tests','state' ]].agg({'n_pass':rounded_mean, 'n_fail':rounded_mean,  'n_tests':rounded_mean, 'state':'unique'})
 
-    stat_prop_result_coverage = stat_prop_result_cv_df.groupby(level=[0,1,2])[['n_exists', 'n_required' , 'n_optional', 'n_extra', 'n_required_exists', 'n_optional_exists', 'coverage' ]].agg({'n_exists':rounded_mean, 'n_required':rounded_mean , 'n_optional':rounded_mean, 'n_extra':rounded_mean, 'n_required_exists':rounded_mean, 'n_optional_exists':rounded_mean, 'coverage':'unique'}) 
+    stat_prop_result_coverage = stat_prop_result_cv_df.groupby(level=[0,1,2])[['n_exists', 'n_required' , 'n_optional', 'n_required_exists', 'n_optional_exists', 'coverage' ]].agg({'n_exists':rounded_mean, 'n_required':rounded_mean , 'n_optional':rounded_mean, 'n_required_exists':rounded_mean, 'n_optional_exists':rounded_mean, 'coverage':'unique'}) 
 
    
     def calc_stage_percentage2( ):
@@ -89,45 +74,75 @@ def main():
        perc_skipped = (((stat_prop_result_cv_df2['n_skipped'] *100) / stat_prop_result_cv_df2['n_total'] ).astype(int)).astype(str)
        perc_failed  = (((stat_prop_result_cv_df2['n_failed'] *100) / stat_prop_result_cv_df2['n_total'] ).astype(int)).astype(str)
 
+       def fillnan_with_zero(x):
+           if(np.isnan(x)):
+              x = 0
+           return x;
+
+           y = fillnan_with_zero(x)
+           y.astyp(str)
+           return y;
+
        perc_result = '['+ perc_success +'%' + ' / '+ perc_skipped +'%'+ ' / '+ perc_failed +'%'+ ']'
        return perc_result;
+
 
     def calc_stage_percentage2_sum( ):
-       perc_success = (((stat_prop_result_cv_df2['n_success'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum() ).astype(int)).astype(str)
-       perc_skipped = (((stat_prop_result_cv_df2['n_skipped'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum() ).astype(int)).astype(str)
-       perc_failed  = (((stat_prop_result_cv_df2['n_failed'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum() ).astype(int)).astype(str)
+       perc_success = (((stat_prop_result_cv_df2['n_success'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum( )).astype(int)).astype(str)
+       perc_skipped = (((stat_prop_result_cv_df2['n_skipped'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum( )).astype(int)).astype(str)
+       perc_failed  = (((stat_prop_result_cv_df2['n_failed'].sum() *100) / stat_prop_result_cv_df2['n_total'].sum()).astype(int)).astype(str)
 
        perc_result = '['+ perc_success +'%' + ' / '+ perc_skipped +'%'+ ' / '+ perc_failed +'%'+ ']'
        return perc_result;
+
 
 
 
     def calc_test_percentage( ):
        perc_pass = (((stat_prop_result_cv_test['n_pass'] *100) / stat_prop_result_cv_test['n_tests']))
        perc_fail = (((stat_prop_result_cv_test['n_fail'] *100) / stat_prop_result_cv_test['n_tests']))
-
-       perc_result = '['+ round(perc_pass,2).astype(str) +'%' + ' / '+ round(perc_fail,2).astype(str) +'%'+ ']'
+       
+#       if(np.isnan(perc_pass)):
+ #         perc_pass = 0; 
+       
+       perc_result = '['+ round(perc_pass,0).astype(str) +'%' + ' / '+ round(perc_fail,0).astype(str) +'%'+ ']'
        return perc_result;
 
     def calc_test_percentage_sum( ):
        perc_pass = (((stat_prop_result_cv_test['n_pass'].sum() *100) / stat_prop_result_cv_test['n_tests'].sum() ).astype(int))
        perc_fail = (((stat_prop_result_cv_test['n_fail'].sum() *100) / stat_prop_result_cv_test['n_tests'].sum() ).astype(int))
 
-       perc_result = '['+ round(perc_pass,2).astype(str) +'%' + ' / '+ round(perc_fail,2).astype(str) +'%'+ ']'
+       perc_result = '['+ round(perc_pass,0).astype(str) +'%' + ' / '+ round(perc_fail,0).astype(str) +'%'+ ']'
        return perc_result;
 
+
+    def calc_coverage_percentage( ):
+       perc_required = (((stat_prop_result_coverage['n_required_exists'] *100) / stat_prop_result_coverage['n_required'] ).astype(int))#.astype(str
+       perc_optional = (((stat_prop_result_coverage['n_optional_exists'] *100) / stat_prop_result_coverage['n_optional'] ).astype(int))#.astype(str)
+       perc_exists  = (((stat_prop_result_coverage['n_exists'] *100) / (stat_prop_result_coverage['n_required']+ stat_prop_result_coverage['n_optional']) ).astype(int))#.astype(str)
+
+       perc_result =  '['+ round(perc_required,0).astype(str) +'%' + ' / '+ round(perc_optional,0).astype(str) +'%'+  ' / '+ round(perc_exists,0).astype(str) +'%]'
+       return perc_result;
+ 
+
     stat_result_summary =  stat_prop_result_cv_df2["n_success"].astype(str)+' / '+ stat_prop_result_cv_df2["n_skipped"].astype(str) +' / '+ stat_prop_result_cv_df2["n_failed"].astype(str)+ ' ' + calc_stage_percentage2()
+
     stat_prop_result_cv_df2.insert(loc=1, column='Status_Summary: (Passed/Skipped/Failed) %', value=stat_result_summary)
 
 
-    stat_prop_test_summary = stat_prop_result_cv_test["n_pass"].astype(str)+' / '+ stat_prop_result_cv_test["n_fail"].astype(str)+ ' ' + calc_test_percentage()
-    stat_prop_result_cv_test.insert(loc=1, column=' Summary Test Results: (Passed/Failed) % ', value=stat_prop_test_summary)
+    stat_test_summary = stat_prop_result_cv_test["n_pass"].astype(str)+' / '+ stat_prop_result_cv_test["n_fail"].astype(str)+ ' ' + calc_test_percentage()
 
-    #'n_exists', 'n_required' , 'n_optional', 'n_extra', 'n_required_exists', 'n_optional_exists'
+    stat_prop_result_cv_test.insert(loc=1, column= "Summary Test Results: (Passed/Failed) % ", value=stat_test_summary)
+    stat_prop_coverage_summary = stat_prop_result_coverage['n_required_exists'].astype(str)+' : '+ stat_prop_result_coverage["n_required"].astype(str)+' / '+stat_prop_result_coverage['n_optional_exists'].astype(str)+' : '+ stat_prop_result_coverage["n_optional"].astype(str) +' / '+stat_prop_result_coverage['n_exists'].astype(str)+ ' : '+ (stat_prop_result_coverage["n_optional"]+stat_prop_result_coverage["n_required"]).astype(str) + ' / '+calc_coverage_percentage()
+
+ #   + (stat_prop_result_coverage["n_optional"]+stat_prop_result_coverage["n_required"]).astype(str)+ 
+
+    stat_prop_result_coverage.insert(loc=1, column=' Summary Test Results Coverage: (No. Existing Required : No. Required / No. Optional Existing : No Optional / No. Total Existing Test : No. Total Tests) % ', value=stat_prop_coverage_summary)
+
 
     final_status_results = stat_prop_result_cv_df2.drop('n_success', axis=1).drop('n_failed', axis=1).drop('n_skipped', axis=1).drop('n_total', axis=1)
     final_test_results = stat_prop_result_cv_test.drop('n_pass', axis=1).drop('n_fail', axis=1).drop('n_tests', axis=1)
-    final_coverage_results = stat_prop_result_coverage.drop('n_exists', axis=1).drop('n_required', axis=1).drop('n_optional', axis=1)
+    final_coverage_results = stat_prop_result_coverage.drop('n_exists', axis=1).drop('n_required', axis=1).drop('n_optional', axis=1).drop('n_required_exists', axis=1).drop('n_optional_exists', axis=1)
    
     final_test_table = pd.merge(final_status_results, final_test_results , on=['model', 'enc_format', 'opcode'], how='inner')
     final_coverage_table = pd.merge(final_test_table, final_coverage_results , on=['model', 'enc_format', 'opcode'], how='inner')
@@ -138,24 +153,25 @@ def main():
 
 
     final_test_table = final_test_table.drop('instr', axis=1)
-    final_coverage_table = final_coverage_table.drop('instr', axis=1)
+    #final_coverage_table = final_coverage_table.drop('instr', axis=1)
     save_data_frames_as_html_to_file(final_test_table, "Grouped_stat_prop_result_test_wo_instr.html")
     save_data_frames_as_html_to_file(final_coverage_table, "Grouped_stat_prop_result_cv_wo_instr.html")
     
 
-    print("FINAL TEST_RESULTS\n")
-    print(final_test_results)
+#   print("FINAL TEST_RESULTS\n")
+#   print(final_test_results)
 
-    print('FINALE STATUS RESULTS\n')
-    print(final_status_results)
-
-
-    print("Final Test Table\n")
-    print(final_test_table)  
+#    print('FINALE STATUS RESULTS\n')
+#    print(final_status_results)
 
 
-    print("Finalt Coverage Table\n")
-    print(final_coverage_table)
+#    print("Final Test Table\n")
+#    print(final_test_table)  
+#    print('finale Coverage Results\n')
+#    print(final_coverage_results)
+#    print("Final Coverage Table\n")
+#    print(final_coverage_table)
 
 if __name__ == "__main__":
     main()
+import argparse
