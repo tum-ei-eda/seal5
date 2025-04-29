c33465f7fa7e6d7849e28b19175743d7b5c71796
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-04-29 10:06:05 +0200

    seal5/transform/filter_model/filter.py: support reserved opcode

--- seal5/transform/filter_model/filter.py
+++ seal5/transform/filter_model/filter.py
@@ -48,7 +48,7 @@ OPCODE_LOOKUP = {
     # "48bit2": 0b10111,
     "BRANCH": 0b11000,
     "JALR": 0b11001,
-    # "reserved": 0b11010,
+    "reserved": 0b11010,
     "JAL": 0b11011,
     "SYSTEM": 0b11100,
     "OP-P": 0b11101,
