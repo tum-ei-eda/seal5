ed315f5bb9c27d47f2595451518b9416cf66dc7a
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-11-28 22:19:19 +0100

    tmp

--- seal5/model.py
+++ seal5/model.py
@@ -196,6 +196,8 @@ class Seal5InstrAttribute(Enum):
     ENC_PATTERN = auto()
     ENC_MASK = auto()
     ENC_MATCH = auto()
+    AUTO_UNROLL_IMM = auto()
+    AUTO_INTRIN = auto()
 
 
 class Seal5FunctionAttribute(Enum):
@@ -215,6 +217,7 @@ class Seal5OperandAttribute(Enum):
     REG_TYPE = auto()
     LLVM_TYPE = auto()
     IS_IMM_LEAF = auto()
+    IS_UNROLL_IMM = auto()
 
 
 class Seal5DataType(Enum):
@@ -237,6 +240,30 @@ class Seal5Type:
         self.datatype = datatype
         self.width = width
         self.lanes = lanes
+
+    @property
+    def is_scalar(self):
+        return self.lanes in [None, 1]
+
+    @property
+    def is_vector(self):
+        return self.lanes is not None and self.lanes > 0
+
+    @property
+    def is_int(self):
+        return self.datatype in [DataType.U, DataType.S]
+
+    @property
+    def is_unsigned_int(self):
+        return self.datatype == DataType.U
+
+    @property
+    def is_signed_int(self):
+        return self.datatype == DataType.S
+
+    @property
+    def is_float(self):
+        return self.datatype in [DataType.F, DataType.D]
 
     def __repr__(self):
         sign_letter = None
