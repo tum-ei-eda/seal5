8adab020469fa2a5d84212a75a933c136f74baa7
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-08-18 10:32:03 +0200

    seal5/transform/infer_types: replace with m2isar upstream version

--- seal5/transform/infer_types/transform.py
+++ seal5/transform/infer_types/transform.py
@@ -6,37 +6,24 @@
 # Chair of Electrical Design Automation
 # Technical University of Munich
 
-"""Remove (rd != 0) checks from M2-ISA-R/Seal5 metamodel."""
+"""Type inference for M2-ISA-R/Seal5 metamodel.
+
+This module uses the upstream M2-ISA-R infer_types implementation which
+provides the modern visitor pattern and type system.
+"""
 
 import sys
 import argparse
 import logging
 import pathlib
 
-from m2isar.metamodel import patch_model
-
 from seal5.model_utils import load_model, dump_model
 
-from . import visitor
+from .visitor import InferTypesMutator, MinimalContext
 
 from seal5.logging import Logger
 
 logger = Logger("transform.infer_types")
-
-
-# TODO: implement full API
-# class TransformAPI(IntEnum):
-#     PYTHON = auto()
-#     CMDLINE = auto()
-#
-#
-# class InferTypes(Seal5Transform):
-#
-#
-#     def setup_parser(self):
-#
-#
-#     def run(self, api: TranformAPI = TranformAPI.PYTHON):
 
 
 def get_parser():
@@ -60,13 +47,14 @@ def run(args):
 
     model_obj = load_model(top_level, compat=args.compat)
 
+    # Process instruction sets (Seal5 specific)
     for _, set_def in model_obj.sets.items():
         logger.debug("inferring types for set %s", set_def.name)
-        patch_model(visitor)
-        # TODO: handle RFS symbolically
+        context = MinimalContext()
+        mutator = InferTypesMutator()
         for _, instr_def in set_def.instructions.items():
             logger.debug("inferring types for instr %s", instr_def.name)
-            instr_def.operation.generate(None)
+            mutator.generate(instr_def.operation, context)
 
     dump_model(model_obj, out_path, compat=args.compat)
 
