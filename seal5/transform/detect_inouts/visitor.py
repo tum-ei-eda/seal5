b9cd59e18d20938eeb0ff3ef882ce3c4aa728169
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-08-18 13:26:31 +0200

    transform/detect_inouts: migrate to M2-ISA-R visitor pattern
    Rework detect_inouts to use the visitor-based ExprVisitor/generate()
    dispatch introduced by the M2-ISA-R metamodel refactor. Relax the
    IndexedReference handling to accept RegisterBank in addition to
    Memory references.

--- seal5/transform/detect_inouts/visitor.py
+++ seal5/transform/detect_inouts/visitor.py
@@ -6,221 +6,163 @@
 # Chair of Electrical Design Automation
 # Technical University of Munich
 
-"""TODO"""
+"""Detect input/output operands through behavioral reads and writes."""
+
+from functools import singledispatchmethod
 
 from m2isar.metamodel import behav
+from m2isar.metamodel.utils.ExprVisitor import ExprVisitor
 
 from .utils import IOMode
 
 # pylint: disable=unused-argument
 
-# Limitations
-# - Keep the condition stack as small as possible (move raise to highest level of behavior)
-# - If block should only have a call to raise
-# - Standalone if statements (without else/else if) are preferred
-# - Raise calls are replaced by empty blocks, to be removed in the operation visitor
-# - ~~Run optimizer afterwards to eliminate if statement.~~
 
+class DetectInoutsVisitor(ExprVisitor):
+    """Collect a set of names read and written by the instruction behavior."""
 
-def operation(self: behav.Operation, context):
-    # print("operation", self)
-    statements = []
-    for stmt in self.statements:
-        temp = stmt.generate(context)
-        if isinstance(temp, list):
-            for _t in temp:
-                pass
-                # print("t", t, type(t), dir(t))
-            statements.extend(temp)
-        else:
-            # print("temp", temp, type(temp), dir(temp))
-            statements.append(temp)
-    # input("eeeee")
+    @singledispatchmethod
+    def generate(self, expr: behav.BaseNode, context):
+        raise NotImplementedError(
+            f"No visit method implemented for type {type(expr).__name__} in {type(self).__name__}"
+        )
 
-    self.statements = statements
-    return self
+    @generate.register
+    def _(self, expr: behav.Operation, context):
+        statements = []
+        for stmt in expr.statements:
+            temp = self.generate(stmt, context)
+            if isinstance(temp, list):
+                statements.extend(temp)
+            else:
+                statements.append(temp)
+        expr.statements = statements
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.Block, context):
+        stmts = []
+        for stmt in expr.statements:
+            stmt = self.generate(stmt, context)
+            if isinstance(stmt, behav.Conditional):
+                if len(stmt.conds) == 1 and isinstance(stmt.stmts[0], behav.Block) and len(stmt.stmts[0].statements) == 0:
+                    continue
+            stmts.append(stmt)
+        expr.statements = stmts
+        return expr
 
-def block(self: behav.Block, context):
-    # print("block", self)
+    @generate.register
+    def _(self, expr: behav.BinaryOperation, context):
+        expr.left = self.generate(expr.left, context)
+        expr.right = self.generate(expr.right, context)
+        return expr
 
-    stmts = []
-
-    for stmt in self.statements:
-        stmt = stmt.generate(context)
-        if isinstance(stmt, behav.Conditional):
-            if len(stmt.conds) == 1:
-                if isinstance(stmt.stmts[0], behav.Block):
-                    if len(stmt.stmts[0].statements) == 0:
-                        continue
-        stmts.append(stmt)
-
-    self.statements = stmts
-
-    return self
-
-
-def binary_operation(self: behav.BinaryOperation, context):
-    # print("binary_operation")
-    self.left = self.left.generate(context)
-    self.right = self.right.generate(context)
-
-    return self
-
-
-def slice_operation(self: behav.SliceOperation, context):
-    # print("slice_operation")
-    self.expr = self.expr.generate(context)
-    context.push(IOMode.READ)
-    self.left = self.left.generate(context)
-    context.pop()
-    context.push(IOMode.READ)
-    self.right = self.right.generate(context)
-    context.pop()
-
-    return self
-
-
-def concat_operation(self: behav.ConcatOperation, context):
-    # print("concat_operation")
-    self.left = self.left.generate(context)
-    self.right = self.right.generate(context)
-
-    return self
-
-
-def number_literal(self: behav.IntLiteral, context):
-    # print("number_literal")
-    return self
-
-
-def int_literal(self: behav.IntLiteral, context):
-    # print("int_literal")
-    return self
-
-
-def scalar_definition(self: behav.ScalarDefinition, context):
-    # print("scalar_definition")
-    return self
-
-
-def assignment(self: behav.Assignment, context):
-    # print("assignment")
-    context.push(IOMode.WRITE)
-    # print("> START WRITE")
-    self.target = self.target.generate(context)
-    # print("< STOP WRITE")
-    context.pop()
-    context.push(IOMode.READ)
-    # print("> START READ")
-    self.expr = self.expr.generate(context)
-    # print("> STOP READ")
-    context.pop()
-
-    return self
-
-
-def conditional(self: behav.Conditional, context):
-    # print("conditional")
-    for i, cond in enumerate(self.conds):
+    @generate.register
+    def _(self, expr: behav.SliceOperation, context):
+        expr.expr = self.generate(expr.expr, context)
         context.push(IOMode.READ)
-        # print("> START READ")
-        self.conds[i] = cond.generate(context)
-        # print("> STOP READ")
+        expr.left = self.generate(expr.left, context)
         context.pop()
-    for op in self.stmts:
-        # for stmt in flatten(op):
-        #    stmt.generate(context)
-        op.generate(context)
+        context.push(IOMode.READ)
+        expr.right = self.generate(expr.right, context)
+        context.pop()
+        return expr
 
-    return self
+    @generate.register
+    def _(self, expr: behav.ConcatOperation, context):
+        expr.left = self.generate(expr.left, context)
+        expr.right = self.generate(expr.right, context)
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.Literal, context):
+        return expr
 
-def loop(self: behav.Loop, context):
-    # print("loop")
-    context.push(IOMode.READ)
-    # print("> START READ")
-    self.cond = self.cond.generate(context)
-    # print("> STOP READ")
-    context.pop()
-    self.stmts = [x.generate(context) for x in self.stmts]
+    @generate.register
+    def _(self, expr: behav.VarDefinition, context):
+        return expr
 
-    return self
+    @generate.register
+    def _(self, expr: behav.Assignment, context):
+        context.push(IOMode.WRITE)
+        expr.target = self.generate(expr.target, context)
+        context.pop()
+        context.push(IOMode.READ)
+        expr.expr = self.generate(expr.expr, context)
+        context.pop()
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.Conditional, context):
+        for i, cond in enumerate(expr.conds):
+            context.push(IOMode.READ)
+            expr.conds[i] = self.generate(cond, context)
+            context.pop()
+        for op in expr.stmts:
+            self.generate(op, context)
+        return expr
 
-def ternary(self: behav.Ternary, context):
-    # print("ternary")
-    context.push(IOMode.READ)
-    # print("> START READ")
-    self.cond = self.cond.generate(context)
-    self.then_expr = self.then_expr.generate(context)
-    self.else_expr = self.else_expr.generate(context)
-    # print("> STOP READ")
-    context.pop()
+    @generate.register
+    def _(self, expr: behav.Loop, context):
+        context.push(IOMode.READ)
+        expr.cond = self.generate(expr.cond, context)
+        context.pop()
+        expr.stmts = [self.generate(x, context) for x in expr.stmts]
+        return expr
 
-    return self
+    @generate.register
+    def _(self, expr: behav.Ternary, context):
+        context.push(IOMode.READ)
+        expr.cond = self.generate(expr.cond, context)
+        expr.then_expr = self.generate(expr.then_expr, context)
+        expr.else_expr = self.generate(expr.else_expr, context)
+        context.pop()
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.Return, context):
+        if expr.expr is not None:
+            expr.expr = self.generate(expr.expr, context)
+        return expr
 
-def return_(self: behav.Return, context):
-    # print("return_")
-    if self.expr is not None:
-        self.expr = self.expr.generate(context)
+    @generate.register
+    def _(self, expr: behav.UnaryOperation, context):
+        expr.right = self.generate(expr.right, context)
+        return expr
 
-    return self
+    @generate.register
+    def _(self, expr: behav.NamedReference, context):
+        if context.is_read:
+            context.reads.add(expr.reference.name)
+        elif context.is_write:
+            context.writes.add(expr.reference.name)
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.IndexedReference, context):
+        expr.index = self.generate(expr.index, context)
+        return expr
 
-def unary_operation(self: behav.UnaryOperation, context):
-    # print("unary_operation")
-    self.right = self.right.generate(context)
+    @generate.register
+    def _(self, expr: behav.TypeConv, context):
+        expr.expr = self.generate(expr.expr, context)
+        return expr
 
-    return self
+    @generate.register
+    def _(self, expr: behav.Callable, context):
+        expr.args = [self.generate(stmt, context) for stmt in expr.args]
+        return expr
 
+    @generate.register
+    def _(self, expr: behav.Group, context):
+        expr.expr = self.generate(expr.expr, context)
+        return expr
 
-def named_reference(self: behav.NamedReference, context):
-    # print("named_reference", self.reference.name)
-    # print("is_read", context.is_read)
-    # print("is_write", context.is_write)
-    if context.is_read:
-        context.reads.add(self.reference.name)
-    elif context.is_write:
-        context.writes.add(self.reference.name)
-    return self
+    @generate.register
+    def _(self, expr: behav.ProcedureCall, context):
+        expr.fn_args = [self.generate(arg, context) for arg in expr.args]
+        return expr
 
-
-def indexed_reference(self: behav.IndexedReference, context):
-    # print("indexed_reference")
-    self.index = self.index.generate(context)
-    return self
-
-
-def type_conv(self: behav.TypeConv, context):
-    # print("type_conv")
-    self.expr = self.expr.generate(context)
-
-    return self
-
-
-def callable_(self: behav.Callable, context):
-    # print("callable_")
-    self.args = [stmt.generate(context) for stmt in self.args]
-
-    return self
-
-
-def group(self: behav.Group, context):
-    # print("group")
-    self.expr = self.expr.generate(context)
-
-    return self
-
-
-def procedure_call(self: behav.ProcedureCall, context):
-    # print("procedure_call")
-
-    self.fn_args = [arg.generate(context) for arg in self.args]
-
-    return self
-
-
-def break_(self: behav.Break, context):
-    return self
+    @generate.register
+    def _(self, expr: behav.Break, context):
+        return expr
